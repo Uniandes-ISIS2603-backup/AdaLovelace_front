610d32a3ca2e21db218350bd7d5ef57d034e2544
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-12-12 16:19:44 -0500

    siu

--- src/app/sale/sale.module.ts
+++ src/app/sale/sale.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SaleService} from "./sale.service";
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from '../app-routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxPermissionsModule} from 'ngx-permissions';
+
+import {SaleListComponent} from './sale-list/sale-list.component';
+import {SaleCreateComponent} from './sale-create/sale-create.component';
+import {SaleDetailComponent} from './sale-detail/sale-detail.component';
+
+
 @NgModule({
   imports: [
-    CommonModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    NgxPermissionsModule
   ],
-  declarations: [],
+  declarations: [SaleListComponent, SaleCreateComponent],
   providers:[SaleService]
 })
 export class SaleModule { }
